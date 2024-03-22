bc73684f922bb89e062a8b677a9e9fe61c8b65ec
Tom Whitwell <tom@whi.tw>
2024-03-22 08:21:38 +0000

    switch to using patches

--- go.mod
+++ go.mod
@@ -5,15 +5,14 @@ go 1.22
 toolchain go1.22.1
 
 require (
+	github.com/bluekeyes/go-gitdiff v0.7.2
+	github.com/charmbracelet/log v0.3.1
 	github.com/urfave/cli/v2 v2.27.1
-	go.uber.org/zap v1.27.0
 )
 
 require (
-	github.com/Shopify/go-lua v0.0.0-20240312125312-5d657e363856 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.9.1 // indirect
-	github.com/charmbracelet/log v0.3.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -24,7 +23,6 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
--- pkg/injector/injector.go
+++ pkg/injector/injector.go
@@ -2,9 +2,11 @@ package injector
 
 import (
 	"bufio"
-	"crypto/md5"
+	"bytes"
 	"embed"
+	"errors"
 	"fmt"
+	"github.com/bluekeyes/go-gitdiff/gitdiff"
 	"github.com/charmbracelet/log"
 	"io"
 	"io/fs"
@@ -21,9 +23,9 @@ const (
 	BalatroTypeMacOS   BalatroType = iota
 )
 
-// Embed the lua files into the executable
+// Embed files into the executable
 //
-//go:embed steamodded
+//go:embed steamodded patches
 var filesToInject embed.FS
 
 type SteamoddedInjector interface {
@@ -31,7 +33,7 @@ type SteamoddedInjector interface {
 	Cleanup() error
 	extractSource() error
 	updateGameLua() error
-	getVersion() ([]string, error)
+	getVersion() (string, error)
 }
 
 func NewInjector(executablePath string) (SteamoddedInjector, error) {
@@ -77,10 +79,10 @@ func (b BalatroInjector) Cleanup() error {
 	return nil
 }
 
-func (b BalatroInjector) getVersion() ([]string, error) {
+func (b BalatroInjector) getVersion() (string, error) {
 	versionFile, err := os.Open(filepath.Join(b.extractPath, "version.jkr"))
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	defer versionFile.Close()
 
@@ -93,11 +95,11 @@ func (b BalatroInjector) getVersion() ([]string, error) {
 			break
 		}
 		if err != nil {
-			return nil, fmt.Errorf("failed to read version file: %w", err)
+			return "", fmt.Errorf("failed to read version file: %w", err)
 		}
 		versions = append(versions, version)
 	}
-	return versions, nil
+	return versions[0], nil
 }
 
 func (b BalatroInjector) injectLuaFiles() error {
@@ -116,32 +118,12 @@ func (b BalatroInjector) injectLuaFiles() error {
 		if d.IsDir() {
 			return nil
 		}
-		//destPath := filepath.Join(b.extractPath, path)
-		//log.Debugf("Injecting %s", destPath)
 
 		sourceFile, err := filesToInject.Open(path)
 		if err != nil {
 			return err
 		}
 		defer sourceFile.Close()
-
-		//// if the file is in a directory, create the directory
-		//if err := os.MkdirAll(filepath.Dir(destPath), 0755); err != nil {
-		//	return err
-		//}
-
-		// Create the file
-		//f, err := os.Create(destPath)
-		//if err != nil {
-		//	return err
-		//}
-		//defer f.Close()
-
-		// Copy the file
-		//_, err = io.Copy(f, sourceFile)
-		//if err != nil {
-		//	return err
-		//}
 
 		// append the file content to the main.lua file
 		_, err = io.Copy(writer, sourceFile)
@@ -161,94 +143,54 @@ func (b BalatroInjector) injectLuaFiles() error {
 		return err
 	}
 	return writer.Flush()
-
-	//mainLuaFilePath := filepath.Join(b.extractPath, "main.lua")
-	//// open the file
-	//f, err := os.OpenFile(mainLuaFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	//if err != nil {
-	//	return err
-	//}
-	//defer f.Close()
-	//
-	//// write out 'dofile' statements for each injected file at the end of the main.lua file
-	//writer := bufio.NewWriter(f)
-	//for _, file := range injectedFiles {
-	//	_, err := writer.WriteString(fmt.Sprintf("love.filesystem.load(\"%s\")()\n", file))
-	//	if err != nil {
-	//		return err
-	//	}
-	//}
-	//return writer.Flush()
 }
 
 func (b BalatroInjector) updateGameLua() error {
-	gameLuaFilePath := filepath.Join(b.extractPath, "game.lua")
+	//gameLuaFilePath := filepath.Join(b.extractPath, "game.lua")
 
-	h := md5.New()
-
-	f, err := os.Open(gameLuaFilePath)
+	version, err := b.getVersion()
 	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if _, err := io.Copy(h, f); err != nil {
-		return err
-	}
-	gameLuaHash := fmt.Sprintf("%x", h.Sum(nil))
-	log.Debugf("game.lua hash: %s", gameLuaHash)
-
-	// retrieve the injection point from gameLuaInjectionPoints map[string]InjectionPoint
-	injectionPoint, ok := gameLuaInjectionPoints[gameLuaHash]
-	if !ok {
-		return fmt.Errorf("could not find injection point for game.lua with hash %s", gameLuaHash)
+		return fmt.Errorf("failed to get version: %w", err)
 	}
 
-	// open the file
-	f, err = os.Open(gameLuaFilePath)
+	patchPath, err := filesToInject.Open(fmt.Sprintf("patches/%s.patch", version))
 	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	// read the file line by line
-	originalLines := make([]string, 0)
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		originalLines = append(originalLines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-
-	var indentedStartCode string
-	for i := 0; i < injectionPoint.Indentation; i++ {
-		indentedStartCode += " "
-	}
-	indentedStartCode += startCode
-
-	// insert the startCode at the correct line and column
-	newLines := originalLines[:injectionPoint.Line-1]
-	newLines = append(newLines, indentedStartCode)
-	newLines = append(newLines, originalLines[injectionPoint.Line:]...)
-
-	// write the modified originalLines back to the file
-	f, err = os.Create(gameLuaFilePath)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	writer := bufio.NewWriter(f)
-	for _, line := range newLines {
-		_, err := writer.WriteString(line + "\n")
-		if err != nil {
-			return err
+		if os.IsNotExist(err) {
+			return fmt.Errorf("Could not find a patch for Balatro version %s. Check if there is a newer version of the injector.", version)
 		}
+		return fmt.Errorf("failed to open patch file: %w", err)
 	}
-	err = writer.Flush()
+	defer patchPath.Close()
+
+	files, _, err := gitdiff.Parse(patchPath)
+	if err != nil {
+		return fmt.Errorf("failed to parse patch file: %w", err)
+	}
+
+	gameLua, err := os.Open(filepath.Join(b.extractPath, "game.lua"))
 	if err != nil {
 		return err
 	}
+	defer gameLua.Close()
+
+	var output bytes.Buffer
+	if err := gitdiff.Apply(&output, gameLua, files[0]); err != nil {
+		log.Debugf("Failed to apply patch: %s", err)
+		if errors.Is(err, &gitdiff.Conflict{}) {
+			return fmt.Errorf("Failed to inject steamodded start code. Is this game already patched?")
+		}
+		return fmt.Errorf("failed to apply patch: %w", err)
+	}
+
+	gameLua, err = os.Create(filepath.Join(b.extractPath, "game.lua"))
+	if err != nil {
+		return err
+	}
+	defer gameLua.Close()
+
+	if _, err := io.Copy(gameLua, &output); err != nil {
+		return fmt.Errorf("failed to write patched game.lua: %w", err)
+	}
+
 	return nil
 }
